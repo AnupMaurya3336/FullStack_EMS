484e9b805d7a712d33b0b78b318f7d7035494eaf
Anup Maurya <anupmaurya3336@gmail.com>
2026-06-13 22:39:01 +0530

    dashboard page UI completed

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Loading = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="animate-spin size-8 rounded-full 
+            border-2 border-indigo-500 border-t-transparent"></div>
+
+        </div>
+    )
+}
+
+export default Loading
